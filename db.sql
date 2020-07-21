2e8d2d3af985b050388ea37904ed8e3171763fef
José Thomaz <jtsoares17@hotmail.com>
2020-07-21 13:28:52 -0300

    Create db.sql

---
+++ db.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE transactions;
+
+ 
+
+USE transactions;
+
+ 
+
+CREATE TABLE users(
+id int not null,
+saldo float not null,
+created_at datetime,
+updated_at datetime,
+PRIMARY KEY(id)
+);
+
+CREATE TABLE extrato(
+id_extrato int not null auto_increment,
+id_conta int not null,
+id_origem int not null,
+id_destino int not null,
+valor decimal(10,2) not null,
+tipo varchar(15) not null,
+data_extrato datetime not null,
+constraint `fk_usuarioExt`
+foreign key(`id_conta`)
+references transactions.users(`id`),
+primary key(`id_extrato`)
+);
